0f90749ce5821c8d8ed76b34d39b3a3b84958744
朱文锦 <Zhu.Wenjin@aliyun.com>
2023-09-11 18:06:53 +0800

    Initial commit

---
+++ LeetCode.Net/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Net;
+
+/*
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ */
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            map[nums[i]] = i;
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (map.TryGetValue(target - nums[i], out int value) && i != value)
+            {
+                return new[] { i, value };
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+}
---
+++ LeetCode.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LeetCode.Test;
+
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Solution solution;
+    }
+}
